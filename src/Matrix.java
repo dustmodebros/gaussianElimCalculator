ce11a54102b16b5de7e6b3ba2ca6a3dbceb5b93d
Samuel Stephens <mistersamstephens@gmail.com>
2023-05-10 01:42:17 +0100

    gaussian elimination in java

---
+++ src/Matrix.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+public class Matrix {
+	ArrayList<ArrayList<Double>> in;
+	public Matrix(ArrayList<ArrayList<Double>> in) {
+		this.in = in;
+	}
+
+	// should multiply the 1st index of in by Xn/X1 of each of the other indices of in
+	public static ArrayList<ArrayList<Double>> triangulate(ArrayList<ArrayList<Double>> in, int pivot){
+		ArrayList<ArrayList<Double>> out = new ArrayList<>();
+		ArrayList<Double> pivotRow = in.get(pivot);
+		Double first = pivotRow.get(pivot);
+		for (int l = 0; l<pivot+1; l++){
+			out.add(in.get(l));
+		}
+		for (int k = pivot; k<in.size(); k++) {
+
+			ArrayList<Double> cheese = in.get(k);
+			if (cheese != pivotRow){
+				Double mul = cheese.get(pivot)/first; //factor to multiply firstRow by to then subtract from cheese
+				ArrayList<Double> temp = new ArrayList<>(pivotRow);
+				temp.replaceAll(aDouble -> aDouble * mul);
+				for (int j = 0; j < temp.size(); j++) {
+					temp.set(j, cheese.get(j)-temp.get(j));
+				}
+				out.add(temp);
+			}
+		}
+		return out;
+	}
+
+
+}
