fcc13ac691ae20bee7ba2169292cc1bc8e355903
Samuel Stephens <mistersamstephens@gmail.com>
2023-05-12 17:04:42 +0100

    logic done

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) {
-		ArrayList<Double> temp1 = new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.0, -1.0));
-		ArrayList<Double> temp2 = new ArrayList<>(Arrays.asList(2.0, 3.0, 4.0, 4.0, 5.0));
-		ArrayList<Double> temp3 = new ArrayList<>(Arrays.asList(-2.0, 2.0, 4.0, 4.0, -2.0));
-		ArrayList<Double> temp4 = new ArrayList<>(Arrays.asList(2.0, -2.0, -4.0, -3.0, 4.0));
-		ArrayList<ArrayList<Double>> temp = new ArrayList<>(Arrays.asList(temp1,temp2,temp3, temp4));
+		ArrayList<Double> temp1 = new ArrayList<>(Arrays.asList(1.0, 2.0, 3.0, 4.0, -1.0, 77.0));
+		ArrayList<Double> temp2 = new ArrayList<>(Arrays.asList(2.0, 3.0, 4.0, 4.0, 5.0, -53.0));
+		ArrayList<Double> temp3 = new ArrayList<>(Arrays.asList(-2.0, 2.0, 4.0, 4.0, -2.0, 86.0));
+		ArrayList<Double> temp4 = new ArrayList<>(Arrays.asList(2.0, -2.0, -4.0, -3.0, 4.0, -117.0));
+		ArrayList<Double> temp5 = new ArrayList<>(Arrays.asList(2.0, -2.0, -14.0, -3.0, 6.0, -211.0));
+		ArrayList<ArrayList<Double>> temp = new ArrayList<>(Arrays.asList(temp1, temp2, temp3, temp4, temp5));
 		Matrix mat = new Matrix(temp);
-		System.out.println(mat);
 		mat.triangulate();
-		System.out.println(mat);
+		System.out.println(mat.returnAnswers());
 	}
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -5,7 +5,6 @@ public class Matrix {
 		this.in = in;
 	}
 
-	// should multiply the 1st index of in by Xn/X1 of each of the other indices of in
 	public void triangulate() { //triangulates :)
 		for (int pivot = 0; pivot <in.size()-1; pivot++) {
 			ArrayList<ArrayList<Double>> out = new ArrayList<>();
@@ -28,6 +27,23 @@ public class Matrix {
 			}
 			in = out;
 		}
+	}
+
+	public ArrayList<Double> returnAnswers(){
+		ArrayList<Double> out = new ArrayList<>();
+		ArrayList<Double> last = in.get(in.size()-1);
+		for (int i = 0; i < last.size()-2; i++) {
+			out.add(0.0);
+		}
+		out.add(last.size()-2, last.get(last.size()-1)/last.get(last.size()-2));
+		for (int j=out.size()-2; j>=0; j--) { //iterate through the arrays from bottom to top
+			ArrayList<Double> current = in.get(j);
+			for (int i = 0; i < out.size(); i++) {
+				current.set(out.size(),current.get(out.size())-(current.get(i)*out.get(i)));
+			}
+			out.set(j, current.get(out.size())/current.get(j));
+		}
+		return out;
 	}
 
 	@Override
